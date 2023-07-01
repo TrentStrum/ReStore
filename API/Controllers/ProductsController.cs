a3b88526f0748a077c3bcbe0e7034cb84e1cd346
tstrum <trent.strum@gmail.com>
2023-07-01 11:11:23 -0700

    End of section 8

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
@@ -13,9 +14,19 @@ namespace API.Controllers
             _context = context;
         }
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts()
+        public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery]ProductParams productParams)
         {
-            return await _context.Products.ToListAsync();
+            var query = _context.Products
+                .Sort(productParams.OrderBy)
+                .Search(productParams.SearchTerm)
+                .Filter(productParams.Brands, productParams.Types)
+                .AsQueryable();
+
+            var products = await PagedList<Product>.ToPagedList(query, productParams.PageNumber, productParams.PageSize);
+
+            Response.AddPaginationHeader(products.MetaData);
+
+            return products;
         }
         
         [HttpGet("{id}")]
@@ -24,6 +35,15 @@ namespace API.Controllers
             var product = await _context.Products.FindAsync(id);
             if (product == null) return NotFound();
             return product;
+        }
+
+        [HttpGet("filters")]
+        public async Task<IActionResult> GetFilters()
+        {
+            var brands = await _context.Products.Select(p => p.Brand).Distinct().ToListAsync();
+            var types = await _context.Products.Select(p => p.Type).Distinct().ToListAsync();
+
+            return Ok(new {brands, types});
         }
     }
 }
